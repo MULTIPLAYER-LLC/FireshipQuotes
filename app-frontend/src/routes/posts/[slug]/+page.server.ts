8b5d49a0b2c29f8a8b18a5a05e7a35b03bdf5af8
Ivan8or <ivanthegreat825@gmail.com>
2025-03-26 21:00:21 -0400

    fix images again

--- app-frontend/src/routes/posts/[slug]/+page.server.ts
+++ app-frontend/src/routes/posts/[slug]/+page.server.ts
@@ -1,10 +1,11 @@
+import { pb } from "$lib/util/pocketbase"; // for helper methods reasons
 import { error } from "@sveltejs/kit";
 
 export async function load({ params, locals }: { params: any, locals: any }) {
   try {
     const postId = params.slug;
     const post = await locals.pb.collection('posts').getOne(postId);
-    const image = locals.pb.files.getURL(post, post?.image)
+    const image = pb.files.getURL(post, post?.image)
     return { post, image };
   }catch(e) {
     console.log(e);
