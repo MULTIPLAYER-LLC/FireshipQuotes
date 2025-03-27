89cf3a249c7d8a62cbe1469458d056c7073cc3e8
Ivan8or <ivanthegreat825@gmail.com>
2025-03-26 20:58:46 -0400

    fix images

--- app-frontend/src/routes/posts/[slug]/+page.server.ts
+++ app-frontend/src/routes/posts/[slug]/+page.server.ts
@@ -4,7 +4,8 @@ export async function load({ params, locals }: { params: any, locals: any }) {
   try {
     const postId = params.slug;
     const post = await locals.pb.collection('posts').getOne(postId);
-    return { post };
+    const image = locals.pb.files.getURL(post, post?.image)
+    return { post, image };
   }catch(e) {
     console.log(e);
     error(404);
--- app-frontend/src/routes/posts/[slug]/+page.svelte
+++ app-frontend/src/routes/posts/[slug]/+page.svelte
@@ -3,19 +3,16 @@
 	import { env } from '$env/dynamic/public';
 
   const { data } = $props();
-  const { post } = data;
+  const { post, image } = data;
 </script>
 
 <svelte:head>
-  <!-- seo -->
-  <title>Fireship Discord quote</title>
-  <meta name="description" content="Quote from the Fireship Discord server">
-  
   <!-- social media display -->
+  <title>Fireship Discord Quotes</title>
   <meta property="og:type" content="article">
   <meta property="og:url" content="{env.PUBLIC_SITE_URL}/posts/{post?.id}">
   <meta property="og:site_name" content="FireshipQuotes">
-  <meta property="og:image" content="{post?.image}">
+  <meta property="og:image" content="{image}">
 </svelte:head>
 
 <article class="max-w-full w-400 mx-auto px-5">
